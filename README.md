20845050c169f0024b678d6ef181ba5f011aa167
Sean <seanhaines13@gmail.com>
2015-06-20 18:02:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Express Survey w/sockets
+Using express to construct a simple survey form. Single view: Index.ejs where users complete the form and the results are appended above the form.
+[Non-sockets version](https://github.com/sseeaann/Express_Survey)
+
+## Node Modules
+- body-parser
+- ejs
+- express
+- socket.io
+
+## Other tools
+- bootstrap
+
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+	path = require("path"),
+	bodyParser = require("body-parser"),
+	app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+var server = app.listen(8000, function(){
+	console.log("Listening on port 8000!");
+});
+
+// have /routes/index.js handle all of our routing
+var route = require("./routes/index.js")(app, server);
